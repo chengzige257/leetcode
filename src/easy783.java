b005de6d197d01972351595f00c8feb1b9553683
chengzige <2572175264@qq.com>
2022-03-08 23:56:09 +0800

    第十一天

---
+++ src/easy783.java
@@ -0,0 +1,63 @@
+import java.util.Stack;
+
+public class easy783 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+//    public int minDiffInBST(TreeNode root) {
+//        int min = Integer.MAX_VALUE;
+//        boolean flag = true;
+//        int pre = 0;
+//        TreeNode p = root;
+//        Stack<TreeNode> stack = new Stack<>();
+//        while(p!=null||!stack.empty()){
+//            while(p!=null){
+//                stack.push(p);
+//                p = p.left;
+//            }
+//
+//            if(!stack.isEmpty()){
+//
+//                p = stack.pop();
+//
+//                if(flag){
+//                    flag = false;
+//                }else{
+//                    min = Math.min(min,p.val-pre);
+//                }
+//                pre = p.val;
+//                p = p.right;
+//            }
+//        }
+//        return min;
+//    }
+    int pre;
+    int min;
+    public int minDiffInBST(TreeNode root){
+        pre = -1;
+        min = Integer.MAX_VALUE;
+        dfs(root);
+        return min;
+    }
+
+    void dfs(TreeNode root){
+        if(root==null)
+            return;
+        dfs(root.left);
+        if (pre != -1) {
+            min = Math.min(min, root.val - pre);
+        }
+        pre = root.val;
+        dfs(root.right);
+    }
+}
---
+++ src/easy872.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+public class easy872 {
+    //叶子相似的树
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        TreeNode root = root1;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        Queue<Integer> queue = new LinkedList<>();
+        while(!stack.empty()){
+            TreeNode pop = stack.pop();
+            if(pop.left==null&&pop.right==null)
+                queue.add(pop.val);
+            if(pop.right!=null){
+                stack.push(pop.right);
+            }
+            if(pop.left!=null)
+                stack.push(pop.left);
+        }
+
+        root = root2;
+        stack.push(root);
+        while(!stack.empty()){
+            TreeNode pop = stack.pop();
+            if(pop.left == null&&pop.right == null){
+                if(queue.isEmpty() || queue.poll() != pop.val)
+                    return false;
+            }
+            if(pop.right!=null){
+                stack.push(pop.right);
+            }
+            if(pop.left!=null)
+                stack.push(pop.left);
+        }
+        return queue.isEmpty();
+    }
+
+//    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+//        List<Integer> seq1 = new ArrayList<Integer>();
+//        if (root1 != null) {
+//            dfs(root1, seq1);
+//        }
+//
+//        List<Integer> seq2 = new ArrayList<Integer>();
+//        if (root2 != null) {
+//            dfs(root2, seq2);
+//        }
+//
+//        return seq1.equals(seq2);
+//    }
+//
+//    public void dfs(TreeNode node, List<Integer> seq) {
+//        if (node.left == null && node.right == null) {
+//            seq.add(node.val);
+//        } else {
+//            if (node.left != null) {
+//                dfs(node.left, seq);
+//            }
+//            if (node.right != null) {
+//                dfs(node.right, seq);
+//            }
+//        }
+//    }
+}
